76e1fe9b5a54f81c5616ac68f21c2de1b44437a0
Stanislav Usenkov <SimSonic@users.noreply.github.com>
2015-03-30 13:23:28 +0600

    Bukkit.getOnlinePlayers

---
+++ src/main/java/ru/simsonic/rscUtilityLibrary/Bukkit/Tools.java
@@ -0,0 +1,31 @@
+package ru.simsonic.rscUtilityLibrary.Bukkit;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public final class Tools
+{
+	public static Set<Player> getOnlinePlayers()
+	{
+		try
+		{
+			final Method method = Bukkit.class.getMethod("getOnlinePlayers", new Class[0]);
+			if(method.getReturnType() == Collection.class)
+			{
+				final Collection<Player> players = (Collection<Player>)method.invoke(null, new Object[0]);
+				return new HashSet<>(players);
+			} else {
+				final Player[] players = (Player[])method.invoke(null, new Object[0]);
+				return new HashSet<>(Arrays.asList(players));
+			}
+		} catch(NoSuchMethodException | InvocationTargetException | IllegalAccessException ex) {
+		}
+		return Collections.emptySet();
+	}
+}
